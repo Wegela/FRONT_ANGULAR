652d45aa2e486169c4e24dbe850b3511b1cf7e3b
Wegela <gwenael.giroux@gmail.com>
2022-10-05 18:10:14 +0200

    eee

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RestaurantIndexComponent} from "./pages/restaurant-index/restaurant-index.component";
+import {RestaurantDetailsComponent} from "./pages/restaurant-details/restaurant-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: '', redirectTo: 'restaurant', pathMatch: "full"
+  },
+  {
+    path: 'restaurant', component: RestaurantIndexComponent
+  },
+  {
+    path: 'restaurant/:id', component: RestaurantDetailsComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestaurantFormComponent } from './components/restaurant-form/restaurant-form.component';
+import { RestaurantTableComponent } from './components/restaurant-table/restaurant-table.component';
+import {HttpClientModule} from "@angular/common/http";
+import { RestaurantIndexComponent } from './pages/restaurant-index/restaurant-index.component';
+import { RestaurantDetailsComponent } from './pages/restaurant-details/restaurant-details.component';
+import {FormsModule} from "@angular/forms";
+import { MoyenneDirective } from './directives/moyenne.directive';
+import { RestaurantCompleteComponent } from './components/restaurant-complete/restaurant-complete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantFormComponent,
+    RestaurantTableComponent,
+    RestaurantIndexComponent,
+    RestaurantDetailsComponent,
+    MoyenneDirective,
+    RestaurantCompleteComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
